fd5cb66407a3ed6db7f9fe99ae07db7bc6fa6c54
by-lucas <lukasmulekezika2@gmail.com>
2022-04-25 20:29:13 -0300

    atualização

--- empresas/views.py
+++ empresas/views.py
@@ -1,7 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
+
+# Alertas de messagens
+from django.contrib import messages
+from django.contrib.messages import constants
+
 from .models import Empresa
 
 class EmpresaCreate(CreateView):
     model = Empresa
     fields = ['nome', ]
+
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario # Funcionario logado
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('OK')
